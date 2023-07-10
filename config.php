115db951be87adf69b6fef99aa768955859d75fe
IchsanHanifdeal <108577976+IchsanHanifdeal@users.noreply.github.com>
2023-07-10 15:06:17 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$conn = oci_connect('username', 'password', 'hostname/service_name');
+$conn = oci_connect('tdsuite', 'allfor1', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.24.128.231)(PORT=1521))(CONNECT_DATA=(SID=KBY)))');
 if (!$conn) {
     $e = oci_error();
     echo "Koneksi Oracle gagal: " . $e['message'];
 } else {
     echo 'Koneksi Oracle berhasil!';
-    oci_close($conn); // Tutup koneksi setelah pengujian
+    oci_close($conn);
 }
 ?>
